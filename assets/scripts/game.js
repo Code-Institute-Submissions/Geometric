bf8d2282da5623f610e61c4072a4eb79302b5850
LiamDHall <hall.d.liam@gmail.com>
2020-10-17 10:59:10 +0000

    game: Made canvas full size of the browser and set background colour
    Turned the canvas into a object.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,10 +2,16 @@ var fullWith = $(window).width();
 var fullHeight = $(window).height();
 
 $( document ).ready(function() {
-    createCanvas();
+    gameCanvas.create();
 });
 
-//Game Area / Start
-function createCanvas() {  //Initial Creation
-    document.body.insertBefore(document.createElement('canvas'), document.body.childNodes[0]);  
+//Game Canvas
+var gameCanvas = {  //Initial Creation
+    canvas : document.createElement('canvas'),
+    create : function() {  //Initial Creation
+        this.canvas.width  = fullWith;
+        this.canvas.height = fullHeight;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
 };
