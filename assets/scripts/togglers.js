ea40c7ed6c30d18045e766a21192fbe4d38bb768
LiamDHall <hall.d.liam@gmail.com>
2020-11-05 20:12:14 +0000

    Add function to toggle through menus

---
+++ assets/scripts/togglers.js
@@ -0,0 +1,10 @@
+// Menu Toggler
+$('.btn--menu').click(function() {
+    $('.menu').removeClass('display-toggle')
+    var menuId =  `#${this.value}`;
+    $(menuId).addClass('display-toggle')
+});
+
+// Hero Colour Toggler
+
+// Laver Colour Toggler
