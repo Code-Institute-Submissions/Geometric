18b4f796601ae61f06552e605770af6881598a00
LiamDHall <hall.d.liam@gmail.com>
2020-11-17 00:17:35 +0000

    Style elements to adapted to screen size

--- assets/scripts/togglers.js
+++ assets/scripts/togglers.js
@@ -20,7 +20,7 @@ var score = {
 // Creates Pause Button when game starts
 function addPause(){
     var headsUp = document.getElementById('headsUp');
-    headsUp.insertAdjacentHTML('afterbegin', '<div id="pause" class="hub_item hub_item--left"><p>P =</p><button id="pauseBtn" class="btn btn--hub">Pause</button></div>');
+    headsUp.insertAdjacentHTML('afterbegin', '<div id="pause" class="hub_item hub_item--left"><p class="hub_text">P =</p><button id="pauseBtn" class="btn btn--hub">Pause</button></div>');
     
     $('#pauseBtn').click(function() {
         pauseGame();
